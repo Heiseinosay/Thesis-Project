687321f0ac31f71603ac38876a4eeec7b3ad1945
Heiseinosay <underbumpbump@gmail.com>
2024-04-25 15:20:35 +0800

    ilovethis

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -3,10 +3,13 @@ import '../style/root.css'
 import '../style/font.css'
 import '../style/upload.css'
 
+import Navigation from '../components/NavigationProgress'
+
 function Upload() {
     return (
         <div>
             <h1 className='inter-bold hello'>Upload</h1>
+            <Navigation active="upload" />
         </div>
     )
 }
