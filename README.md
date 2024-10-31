1dc06c380456c7317d14ea9bf212d412995720b9
zitch8 <darendasallas27@gmail.com>
2024-11-01 05:18:33 +0800

    x_train.shape[1] problem

---
+++ README.md
@@ -0,0 +1,13 @@
+# Setup
+1. cd to project's root directory
+2. Install dependencies
+```
+npm install
+```
+3. Run the server
+```
+npm start
+```
+4. cd to server
+
+5. type -> python main.py
---
+++ src/pages/Upload.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+import '../style/root.css'
+import '../style/font.css'
+import '../style/upload.css'
+
+import Navigation from '../components/NavigationProgress'
+import DropZone from '../components/DragDropFiles'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
+
+import axios from "axios";
+
+function Upload() {
+    return (
+        <div className='upload-body'>
+            <div className="column column-first">
+                <div className="restrictions">
+                    <h1 className='inter-bold'>Restrictions</h1>
+                    <p className='inter-light'><FontAwesomeIcon className='icon' icon={faCircleInfo} />File size 50 mb</p>
+                    <p className='inter-light'><FontAwesomeIcon className='icon' icon={faCircleInfo} />File format .wav, .mp3,. flac, etc.</p>
+                </div>
+            </div>
+            <div className="column column-midle">
+                <h1 className='inter-bold'>Verify Audio</h1>
+                <DropZone />
+            </div>
+            <div className="column"></div>
+            <Navigation active={1} />
+        </div>
+    )
+}
+
+export default Upload
